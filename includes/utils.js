79fe44d5be934c70c598a1d5f72165fbba3896d7
Moacy Barro <moacy.barro@drivenbrands.com>
2023-07-12 18:33:57 +0000

    Add example of references/dependencies with full qualifier

--- includes/utils.js
+++ includes/utils.js
@@ -35,7 +35,39 @@ function generateQuery(tableName) {
   return query;
 }
 
-module.exports = { generateQuery };
+function doc_table(tableName) {
+  const tb_name = tableName.split('.').pop().replace('`', '')
+  var json = require('includes/schemas/'+tb_name+'.json');
+  const jsonMetadata = json
+
+  const columns = Object.keys(jsonMetadata);
+
+  const result = {};
+  result = columns.forEach(column => {
+      result[column.name] = column.description;
+    });
+   
+  columns = {
+    user_id: `A unique identifier for a user`,
+    age: `The age of a user`,
+    creation_date: `The date this user signed up`,
+    user_tenure: `The number of years since the user's creation date`,
+    badge_count: `The all-time number of badges the user has received`,
+    questions_and_answer_count: `The all-time number of questions and answers the user has created`,
+    question_count: `The all-time number of questions the user has created`,
+    answer_count: `The all-time number of answers the user has created`,
+    last_badge_received_at: `The time the user received their most recent badge`,
+    last_posted_at: `The time the user last posted a question or answer`,
+    last_question_posted_at: `The time the user last posted an answer`,
+    last_answer_posted_at: `The time the user last posted a question`,
+}
+  
+  // return result
+  return columns
+}
+
+
+module.exports = { generateQuery, doc_table };
 
 
 
